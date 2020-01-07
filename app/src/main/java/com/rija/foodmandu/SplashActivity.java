f410d599c04349c315d2508bcd8ddf20c94158b3
Rija Khatri <kcrijakhatri31@gmail.com>
2020-01-07 09:07:13 +0545

    Initial commit

---
+++ app/src/main/java/com/rija/foodmandu/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.rija.foodmandu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.rija.foodmandu.ui.LoginActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1000);
+    }
+}
---
+++ app/src/main/java/com/rija/foodmandu/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.rija.foodmandu.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rija.foodmandu.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
